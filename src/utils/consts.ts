7397cffda8646e51a56589cdafe145ecd90e0eab
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-08-27 18:32:18 +0530

    adding consts for socials section

---
+++ src/utils/consts.ts
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const SocialsList = [
+    {
+        title: 'twitter / X',
+        image: 'twitter.png'
+    },
+    {
+        title: 'instagram',
+        image: 'instagram.png'
+    },
+    {
+        title: 'snapchat',
+        image: 'snapchat.png'
+    },
+    {
+        title: 'tiktok',
+        image: 'tiktok.png'
+    },
+    {
+        title: 'youtube',
+        image: 'youtube.png'
+    },
+]
